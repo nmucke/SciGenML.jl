5fa2017eb751ae858e066d4e8fa970724dfc94c9
ntm <nmucke@gmail.com>
2025-04-18 15:19:38 +0200

    Update documentation and remove ScoreBasedModel from Models module

--- docs/src/index.md
+++ docs/src/index.md
@@ -10,5 +10,13 @@ Documentation for [SciGenML](https://github.com/nmucke/SciGenML.jl).
 ```
 
 ```@autodocs
-Modules = [SciGenML]
+Modules = [
+    SciGenML, 
+    SciGenML.Models, 
+    SciGenML.NeuralNetworkArchitectures, 
+    SciGenML.NeuralNetworkLayers, 
+    SciGenML.TimeIntegrators, 
+    SciGenML.Data, 
+    SciGenML.Sampling
+]
 ```
--- src/models/Models.jl
+++ src/models/Models.jl
@@ -1,26 +1,11 @@
 module Models
 
-
 export GenerativeModel
-export ScoreBasedModel
 
 """
 Abstract type for all generative models.
 """
 abstract type GenerativeModel end
-
-"""
-Score-based generative model.
-"""
-struct ScoreBasedModel <: GenerativeModel 
-    model_name::String
-    model_type::String
-    model_path::String
-
-    function ScoreBasedModel()
-        return new("ScoreBasedModel", "score_based", "models/score_based")
-    end
-end
 
 
 end
