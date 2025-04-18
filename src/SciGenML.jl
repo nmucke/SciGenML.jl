63719270e5d932f20562f63cdea64fbd76010aaf
nmucke <45429448+nmucke@users.noreply.github.com>
2025-04-18 15:10:55 +0200

    Set-up-base-structure (#2)
    * Add modules for neural network architectures, models, time integrators, and data; update Project.toml dependencies; and create initial test setup.
    
    * Refactor SciGenML module by removing unused exports and cleaning up test setup; streamline TimeIntegrators.jl implementation.
    
    * Add sampling module and update test setup to verify module loading; include Sampling.jl in SciGenML.jl

--- src/SciGenML.jl
+++ src/SciGenML.jl
@@ -1,5 +1,14 @@
 module SciGenML
 
-# Write your package code here.
+include("models/Models.jl")
 
+include("neural_network_architectures/NeuralNetworkArchitectures.jl")
+
+include("neural_network_layers/NeuralNetworkLayers.jl")
+
+include("time_integrators/TimeIntegrators.jl")
+
+include("data/Data.jl")
+
+include("sampling/Sampling.jl")
 end
---
+++ test/load_modules.jl
@@ -0,0 +1,12 @@
+
+
+@testitem "Submodule Loading" begin
+    using SciGenML
+    @test isdefined(SciGenML, :Models)
+    @test isdefined(SciGenML, :NeuralNetworkArchitectures)
+    @test isdefined(SciGenML, :NeuralNetworkLayers)
+    @test isdefined(SciGenML, :TimeIntegrators)
+    @test isdefined(SciGenML, :Data)
+    @test isdefined(SciGenML, :Sampling)
+end
+
