9b92be209b5719a7015faa259d01988c7c36d4ac
Documenter.jl <documenter@juliadocs.github.io>
2025-04-18 09:41:38 +0000

    build based on bbc72a0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SciGenML","category":"page"},{"location":"#SciGenML","page":"Home","title":"SciGenML","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SciGenML.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SciGenML]","category":"page"}]
+}
